d49ee3d548b60ebe04d1bd51947fe41b21d2f4cb
João Victor da Silva Coutinho <joao.coutinho@compasso.com.br>
2023-03-26 17:03:35 -0300

    Del: extend-expect from calculator since it is in jest config

--- src/__tests__/calculator.js
+++ src/__tests__/calculator.js
@@ -1,5 +1,3 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import '@testing-library/jest-dom/extend-expect'
 import React from 'react'
 import {fireEvent, render} from '@testing-library/react'
 import Calculator from '../calculator'
